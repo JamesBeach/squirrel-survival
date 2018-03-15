3f528c7fced494eb76b6a3f32041a3f5ac2bd83c
JamesBeach <andrewb@uoregon.edu>
2018-03-15 12:49:43 -0700

    Bring mapping and campaign level controller fix to master

--- Assets/Scripts/Game/CampaignLevelController.cs
+++ Assets/Scripts/Game/CampaignLevelController.cs
@@ -59,7 +59,7 @@ public class CampaignLevelController : LevelController
     }
 
     // Use this for initialization
-    protected void Start()
+    protected override void Start()
 	{
         base.Start();
 
